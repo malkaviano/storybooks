b297743c9dcfbea22b1610d99c010bb84d3694c6
Malkaviano <rafael.kss@gmail.com>
2017-11-21 23:04:47 +0000

    test

--- config/database.js
+++ config/database.js
@@ -1,11 +1,5 @@
 'use strict';
 
-if(process.env.NODE_ENV === 'production') {
-  module.exports = {
-    mongoURI: 'mongodb://vidjot_user:vidjotmongo@ds113775.mlab.com:13775/vidjot-prod'
-  }
-} else {
-  module.exports = {
-    mongoURI: 'mongodb://story_user:storymongo@ds119355.mlab.com:19355/storybooks-dev'
-  }
+module.exports = {
+  mongoURI: 'mongodb://story_user:storymongo@ds119355.mlab.com:19355/storybooks-dev'
 }
