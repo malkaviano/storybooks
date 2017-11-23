45a2309a744dc724f495a58c3b9d7dc7bbf254af
Malkaviano <rafael.kss@gmail.com>
2017-11-23 20:41:35 +0000

    Stories

--- helpers/authenticate.js
+++ helpers/authenticate.js
@@ -5,9 +5,13 @@ const defaults = require('../config/defaults.json');
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
     if (req.session.username) {
+      console.log(req.session.username);
+
       return next();
     } else {
       req.session.requestedUrl = req.originalUrl;
+
+      console.log(req.session.requestedUrl);
 
       res.redirect(defaults.loginUrl); 
     }       
