0043ef9d400b887a95952b42ff05b431672667a8
Malkaviano <rafael.kss@gmail.com>
2017-11-24 20:35:37 +0000

    Refactoring

--- models/story.js
+++ models/story.js
@@ -1,73 +1,76 @@
 'use strict';
 
-const mongoose = require('../config/mongodb');
+function schema() {
+  const Schema = mongoose.Schema;
 
-module.exports = {
-  model: (function() {
-    const Schema = mongoose.Schema;
-
-    return mongoose.model(
-      'story',
-      new Schema(
-        {
-          title: {
+  return mongoose.model(
+    'story',
+    new Schema(
+      {
+        title: {
+          type: String,
+          required: true,
+          minlength: 5,
+          maxlength: 20,
+          trim: true
+        },
+        status: {
+          type: String,
+          default: 'public'
+        },
+        description: {
+          type: String,
+          required: true,
+          minlength: 50,
+          maxlength: 1000,
+          trim: true
+        },
+        allowComments: {
+          type: Boolean,
+          default: true
+        },
+        comments: [{
+          commentText: {
             type: String,
             required: true,
-            minlength: 5,
-            maxlength: 20,
+            minlength: 10,
+            maxlength: 200,
             trim: true
           },
-          status: {
-            type: String,
-            default: 'public'
+          commentDate: {
+            type: Date,
+            default: Date.now
           },
-          description: {
-            type: String,
-            required: true,
-            minlength: 50,
-            maxlength: 1000,
-            trim: true
-          },
-          allowComments: {
-            type: Boolean,
-            default: true
-          },
-          comments: [{
-            commentText: {
-              type: String,
-              required: true,
-              minlength: 10,
-              maxlength: 200,
-              trim: true
-            },
-            commentDate: {
-              type: Date,
-              default: Date.now
-            },
-            commentUser: {
-              type: Schema.Types.ObjectId,
-              ref: 'user',
-              required: true
-            }
-          }],
-          author: {
+          commentUser: {
             type: Schema.Types.ObjectId,
             ref: 'user',
             required: true
-          },
-          date: {
-            type: Date,
-            default: Date.now
           }
+        }],
+        author: {
+          type: Schema.Types.ObjectId,
+          ref: 'user',
+          required: true
+        },
+        date: {
+          type: Date,
+          default: Date.now
         }
-      ),
-      'stories'
-    );
-  })(),
+      }
+    ),
+    'stories'
+  );
+};
+
+const mongoose = require('../config/mongodb'),
+      model = schema();
+
+module.exports = {
+  model: model,
   helper: {
     findUserStory: (id, userId) => {
-      return this.model.findOne({ _id: id, author: userId })
-                        .populate('author');
+      return model.findOne({ _id: id, author: userId })
+                  .populate('author');
     }
   }
 };
