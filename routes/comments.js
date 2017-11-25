ad931b1ef42e13ea5afc4f1b4b2374cdd29e6800
Malkaviano <rafael.kss@gmail.com>
2017-11-25 04:12:32 +0000

    Refactoring

--- routes/comments.js
+++ routes/comments.js
@@ -15,11 +15,14 @@ function registerRoutes() {
           commentText: req.body.commentText,
           commentUser: req.session.userId
         }
+        
+        story.comments.push(newComment);
+
         utils.resolvePromise(
           story.save(),
           saved => {
             res.flash('info_msg', 'Comment saved');
-            
+
             res.redirect(`/stories/${req.params.storyId}`);
           },
           err => {
