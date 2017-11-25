891dd461db4e721f7e96ee3cfe72eab9e5513a55
Malkaviano <rafael.kss@gmail.com>
2017-11-25 15:08:17 +0000

    Refactoring

--- config/handlebars.js
+++ config/handlebars.js
@@ -1,19 +1,22 @@
 'use strict';
 
 const exphbs = require('express-handlebars'),
-      {truncate, formatDate, selected, htmlToText} = require('../helpers/hbs');
+      hbsHelper = require('../helpers/hbs');
 
 module.exports = {
   config: function() {
-    return exphbs({
-      helpers: {
-      truncate: truncate,
-      formatDate: formatDate,
-      selected: selected,
-      htmlToText: htmlToText
-      },
-      defaultLayout: 'main' 
-    });
+    return exphbs(
+      {
+        helpers: {
+          truncate: hbsHelper.truncate,
+          formatDate: hbsHelper.formatDate,
+          selected: hbsHelper.selected,
+          htmlToText: hbsHelper.htmlToText,
+          showEditIcon: hbsHelper.showEditIcon
+        },
+        defaultLayout: 'main' 
+      }
+    );
   },
   name: 'handlebars'
 };
