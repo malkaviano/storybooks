c03b6e0a7b232df6aed56ca16a18ae61cdd337a9
Malkaviano <rafael.kss@gmail.com>
2017-12-11 21:50:20 +0000

    Redis session store

--- config/redis.js
+++ config/redis.js
@@ -8,8 +8,8 @@ const redis = new Redis({
   db: 0  
 });
 
-client.set('my test key', 'my test value', redis.print);
-client.get('my test key', function(error, result) {
+redis.set('my test key', 'my test value', redis.print);
+redis.get('my test key', function(error, result) {
   if (error) throw error;
   console.log('GET result ->', result)
 });
